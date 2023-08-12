1c2d4cc1c11a91ca97b547cb24507fa6044ebf95
Darshan <darshanpanchal557@gmail.com>
2023-08-12 21:55:17 +0530

    final commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,33 +1,22 @@
 const ErrorHandler = require('../utils/errorhandler');
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
 const axios = require("axios");
-const geolib = require("geolib");
+
 
 exports.getCoordinates = catchAsyncErrors(async (req, res, next) => {
     const { currentLocation,pickupLocation } = req.params;
-    const current = await axios.get(`http://nominatim.openstreetmap.org/search?format=json&q=${currentLocation}`);
-    const pickup = await axios.get(`http://nominatim.openstreetmap.org/search?format=json&q=${pickupLocation}`);
-    
-    console.log(current.data[0]);
-    console.log(pickup.data[0]);
-    const currentCoords = current.data[0];
-    const pickupCoords = pickup.data[0];
+    const apiKey ='5b3ce3597851110001cf624858cef959b1ba472bb282fd0206edc847';
+    const url = `https://api.openrouteservice.org/v2/directions/driving-car?api_key=${apiKey}&start=${currentLocation}&end=${pickupLocation}`;
 
-    const numberOfPoints = 20;
-
-    const interpolatedCoords = [];
-
-    for (let i = 0; i <= numberOfPoints; i++) {
-        const latitude = currentCoords.lat + ((pickupCoords.lat - currentCoords.lat) / numberOfPoints) * i;
-        const longitude = currentCoords.lon + ((pickupCoords.lon - currentCoords.lon) / numberOfPoints) * i;
-        console.log(`Interpolated point ${i}: Lat ${latitude}, Lon ${longitude}`);
-        interpolatedCoords.push({ latitude, longitude });
+    try {
+        const response = await axios.get(url);
+        const steps = response.data.features[0].geometry.coordinates;
+        res.status(200).send(steps.map(coord => ({
+            lat: coord[1],
+            lng: coord[0]
+        })));
+    } catch (error) {
+        console.error('Error fetching route:', error);
     }
-
-    const jsonCoordinates = JSON.stringify(interpolatedCoords, null, 2);
-    res.status(200).send({
-        success:true,
-        data:jsonCoordinates
-    })
 })
 
